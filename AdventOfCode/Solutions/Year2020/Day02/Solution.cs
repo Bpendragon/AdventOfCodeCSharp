b38ec5ce3e241bc9b90c3661a75721a0ca87ace4
Bpendragon (David Camp) <david@campclan.net>
2020-12-01 21:25:30 -0800

    Day 2

--- AdventOfCode/Solutions/Year2020/Day02/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day02/Solution.cs
@@ -1,25 +1,51 @@
 using System.Collections.Generic;
 using AdventOfCode.UserClasses;
+using System;
+using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2020
 {
 
     class Day02 : ASolution
     {
-
+        List<string> passwords;
         public Day02() : base(02, 2020, "")
         {
-
+            passwords = new List<string>(Input.SplitByNewline());
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            int validPass = 0;
+            foreach (var password in passwords)
+            {
+                string[] splitline = password.Split(new char[] { ' ', ':', '-' });
+                int minCount = int.Parse(splitline[0]);
+                int maxcount = int.Parse(splitline[1]);
+                char check = splitline[2][0];
+
+                int count = splitline[^1].Count(x => (x == check));
+                if (count >= minCount && count <= maxcount) validPass++;
+            }
+
+            return validPass.ToString();
+
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            int validPass = 0;
+            foreach (var password in passwords)
+            {
+                string[] splitline = password.Split(new char[] { ' ', ':', '-' });
+                int minCount = int.Parse(splitline[0]);
+                int maxcount = int.Parse(splitline[1]);
+                char check = splitline[2][0];
+
+                if (check == splitline[^1][minCount - 1] ^ splitline[^1][maxcount - 1] == check) validPass++;
+            }
+
+            return validPass.ToString();
         }
     }
 }
