b3e05d1b316ee931cddb0afcd48f1f4f7da0bc34
Bpendragon (David Camp) <david@campclan.net>
2020-12-04 21:24:11 -0800

    2020 day 5, quick dirty and binary

--- AdventOfCode/Solutions/Year2020/Day05/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day05/Solution.cs
@@ -9,19 +9,38 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day05 : ASolution
     {
-
+        List<string> passes;
+        List<int> passIDs;
         public Day05() : base(05, 2020, "")
         {
+            passes = new List<string>(Input.SplitByNewline());
+            passIDs = new List<int>();
+            foreach (var pass in passes)
+            {
+                var row = pass.Substring(0, 7);
+                var seat = pass[^3..];
+                row = row.Replace('F', '0').Replace('B', '1');
+                seat = seat.Replace('R', '1').Replace('L', '0');
+                int test = (Convert.ToInt32(row, 2) * 8) + Convert.ToInt32(seat, 2);
+                passIDs.Add(test);
+
+            }
+
+            passIDs.Sort();
 
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            return passIDs.Last().ToString();
         }
 
         protected override string SolvePartTwo()
         {
+            for(int i = 1; i < passIDs.Count - 1; i++)
+            {
+                if (!(passIDs[i - 1] == (passIDs[i] - 1) && passIDs[i + 1] == (passIDs[i] + 1))) return (passIDs[i] + 1).ToString();
+            }
             return null;
         }
     }
