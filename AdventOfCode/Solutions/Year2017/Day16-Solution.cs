9ab9777e1aec511378cc5710322ab56785f48874
Bpendragon (David Camp) <Bpendragon@users.noreply.github.com>
2020-12-10 21:30:45 -0800

    starting 2017/16

--- AdventOfCode/Solutions/Year2017/Day16-Solution.cs
+++ AdventOfCode/Solutions/Year2017/Day16-Solution.cs
@@ -9,19 +9,54 @@ namespace AdventOfCode.Solutions.Year2017
 
     class Day16 : ASolution
     {
+        readonly List<string> danceMoves = new List<string>();
+        List<char> dancers = new List<char>() { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p' };
+        readonly List<char> dancers2 = new List<char>() { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p' };
         public Day16() : base(16, 2017, "")
         {
-
+            danceMoves.AddRange(Input.Split(','));
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            foreach (var move in danceMoves) Dance(move);
+            return dancers.JoinAsStrings();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+
+            foreach (long i in Enumerable.Range(0, 100000)) foreach (var move in danceMoves) Dance(move);
+            return dancers.JoinAsStrings();
+        }
+
+        private void Dance(string move)
+        {
+            var splits = move[1..].Split('/');
+            char tmp;
+            int a;
+            int b;
+            switch (move[0])
+            {
+                case 's':
+                    a = int.Parse(splits[0]);
+                    dancers = dancers.Rotate(-a).ToList();
+                    break;
+                case 'x':
+                    a = int.Parse(splits[0]);
+                    b = int.Parse(splits[1]);
+                    tmp = dancers[a];
+                    dancers[a] = dancers[b];
+                    dancers[b] = tmp;
+                    break;
+                case 'p':
+                    a = dancers.IndexOf(splits[0][0]);
+                    b = dancers.IndexOf(splits[1][0]);
+                    tmp = dancers[a];
+                    dancers[a] = dancers[b];
+                    dancers[b] = tmp;
+                    break;
+            }
         }
     }
 }
