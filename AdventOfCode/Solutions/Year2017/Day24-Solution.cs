5f57e059bc68e001fd9d96bdbf455ed6b144bfa9
Bpendragon (David Camp) <Bpendragon@users.noreply.github.com>
2020-12-14 00:04:47 -0800

    2017 completed

--- AdventOfCode/Solutions/Year2017/Day24-Solution.cs
+++ AdventOfCode/Solutions/Year2017/Day24-Solution.cs
@@ -9,19 +9,69 @@ namespace AdventOfCode.Solutions.Year2017
 
     class Day24 : ASolution
     {
+        List<int> BridgeScores = new List<int>();
+        List<(int, int)> BridgeLengths = new List<(int, int)>();
+        List<BridgeComponent> pieces = new List<BridgeComponent>();
         public Day24() : base(24, 2017, "")
         {
-
+            int i = 0;
+            foreach(var line in Input.SplitByNewline())
+            {
+                var tmp = line.Split('/');
+                pieces.Add(new BridgeComponent()
+                {
+                    id = i,
+                    a = int.Parse(tmp[0]),
+                    b = int.Parse(tmp[1])
+                });
+                i++;
+            }
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            BuildBridge(0, pieces, 0, 0);
+            return BridgeScores.Max().ToString();
         }
+
+       
 
         protected override string SolvePartTwo()
         {
-            return null;
+            BridgeLengths.Sort((a, b) => a.Item1.CompareTo(b.Item1));
+
+            return BridgeLengths.Where(x => x.Item1 == BridgeLengths.Last().Item1).Max(c => c.Item2).ToString();
+        }
+
+        private void BuildBridge(int lookingFor, List<BridgeComponent> availPieces, int currentScore, int depth)
+        {
+            var candidates = availPieces.Where(x => x.a == lookingFor || x.b == lookingFor).ToList();
+            BridgeLengths.Add((depth, currentScore));
+            foreach(var item in candidates)
+            {
+                int nxtScore = currentScore + item.Score;
+                BridgeScores.Add(nxtScore);
+
+                var nxtAvail = new List<BridgeComponent>(availPieces);
+                nxtAvail.RemoveAll(x => x.id == item.id);
+                if(item.a == lookingFor)
+                {
+                    BuildBridge(item.b, nxtAvail, nxtScore, depth + 1);
+                } else
+                {
+                    BuildBridge(item.a, nxtAvail, nxtScore, depth + 1);
+                }
+            }
+            
+        }
+
+        internal class BridgeComponent
+        {
+            public int id;
+            public int a;
+            public int b;
+
+            public int Score => a + b;
         }
     }
 }
--- AdventOfCode/Solutions/Year2017/Day25-Solution.cs
+++ AdventOfCode/Solutions/Year2017/Day25-Solution.cs
@@ -16,12 +16,132 @@ namespace AdventOfCode.Solutions.Year2017
 
         protected override string SolvePartOne()
         {
-            return null;
+            TuringMachine tm = new TuringMachine();
+
+            foreach (int _ in Enumerable.Range(0, 12629077)) tm.Step();
+
+            return tm.Tape.Where(x => x.Value == 1).Count().ToString();
         }
 
         protected override string SolvePartTwo()
         {
             return "❄️🎄Happy Advent of Code🎄❄️";
         }
+    }
+
+    public class TuringMachine
+    {
+        //This entire thing is handcoded from my input. It probably won't work for you.
+        public Dictionary<int, int> Tape = new Dictionary<int, int>();
+        private int Cursor = 0;
+        private States CurrentState = States.A;
+
+        internal enum States
+        {
+            A,
+            B,
+            C,
+            D,
+            E,
+            F
+        }
+
+        public TuringMachine ()
+        {
+            Tape[Cursor] = 0;
+        }
+
+        public void Step()
+        {
+            if (!Tape.ContainsKey(Cursor)) Tape[Cursor] = 0;
+            switch(CurrentState)
+            {
+                case States.A:
+                    if (Tape[Cursor] == 0)
+                    {
+                        Tape[Cursor] = 1;
+                        Cursor++;
+                        CurrentState = States.B;
+                    } else
+                    {
+                        Tape[Cursor] = 0;
+                        Cursor--;
+                        CurrentState = States.B;
+                    }
+                    break;
+                case States.B:
+                    if (Tape[Cursor] == 0)
+                    {
+                        Tape[Cursor] = 0;
+                        Cursor++;
+                        CurrentState = States.C;
+                    }
+                    else
+                    {
+                        Tape[Cursor] = 1;
+                        Cursor--;
+                        CurrentState = States.B;
+                    }
+                    break;
+                case States.C:
+                    if (Tape[Cursor] == 0)
+                    {
+                        Tape[Cursor] = 1;
+                        Cursor++;
+                        CurrentState = States.D;
+                    }
+                    else
+                    {
+                        Tape[Cursor] = 0;
+                        Cursor--;
+                        CurrentState = States.A;
+                    }
+                    break;
+                case States.D:
+                    if (Tape[Cursor] == 0)
+                    {
+                        Tape[Cursor] = 1;
+                        Cursor--;
+                        CurrentState = States.E;
+                    }
+                    else
+                    {
+                        Tape[Cursor] = 1;
+                        Cursor--;
+                        CurrentState = States.F;
+                    }
+                    break;
+                case States.E:
+                    if (Tape[Cursor] == 0)
+                    {
+                        Tape[Cursor] = 1;
+                        Cursor--;
+                        CurrentState = States.A;
+                    }
+                    else
+                    {
+                        Tape[Cursor] = 0;
+                        Cursor--;
+                        CurrentState = States.D;
+                    }
+                    break;
+                case States.F:
+                    if (Tape[Cursor] == 0)
+                    {
+                        Tape[Cursor] = 1;
+                        Cursor++;
+                        CurrentState = States.A;
+                    }
+                    else
+                    {
+                        Tape[Cursor] = 1;
+                        Cursor--;
+                        CurrentState = States.E;
+                    }
+                    break;
+            }
+        }
+
+
     }
 }
