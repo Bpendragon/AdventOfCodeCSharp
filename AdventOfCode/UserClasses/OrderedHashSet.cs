1d62aa6dae375ad43eff5127e0d7099fe4724841
Bpendragon (David Camp) <Bpendragon@users.noreply.github.com>
2020-12-15 20:54:25 -0800

    Added OrderedHashSet

---
+++ AdventOfCode/UserClasses/OrderedHashSet.cs
@@ -0,0 +1,86 @@
+﻿// This code is distributed under MIT license. Copyright (c) 2013 George Mamaladze
+// See license.txt or http://opensource.org/licenses/mit-license.php
+// Original code at: https://gist.github.com/gmamaladze/3d60c127025c991a087e
+
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AdventOfCode.UserClasses.DataStructures
+{
+    public class OrderedHashSet<T> : ICollection<T>
+    {
+        private readonly IDictionary<T, LinkedListNode<T>> m_Dictionary;
+        private readonly LinkedList<T> m_LinkedList;
+
+        public OrderedHashSet()
+            : this(EqualityComparer<T>.Default)
+        {
+        }
+
+        public OrderedHashSet(IEqualityComparer<T> comparer)
+        {
+            m_Dictionary = new Dictionary<T, LinkedListNode<T>>(comparer);
+            m_LinkedList = new LinkedList<T>();
+        }
+
+        public int Count
+        {
+            get { return m_Dictionary.Count; }
+        }
+
+        public virtual bool IsReadOnly
+        {
+            get { return m_Dictionary.IsReadOnly; }
+        }
+
+        void ICollection<T>.Add(T item)
+        {
+            Add(item);
+        }
+
+        public void Clear()
+        {
+            m_LinkedList.Clear();
+            m_Dictionary.Clear();
+        }
+
+        public bool Remove(T item)
+        {
+            LinkedListNode<T> node;
+            bool found = m_Dictionary.TryGetValue(item, out node);
+            if (!found) return false;
+            m_Dictionary.Remove(item);
+            m_LinkedList.Remove(node);
+            return true;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return m_LinkedList.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public bool Contains(T item)
+        {
+            return m_Dictionary.ContainsKey(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            m_LinkedList.CopyTo(array, arrayIndex);
+        }
+
+        public bool Add(T item)
+        {
+            if (m_Dictionary.ContainsKey(item)) return false;
+            LinkedListNode<T> node = m_LinkedList.AddLast(item);
+            m_Dictionary.Add(item, node);
+            return true;
+        }
+    }
+}
