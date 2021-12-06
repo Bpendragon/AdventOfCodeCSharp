2ec9bdfd7c886316aac671d36619a18c3a7cad7a
David Camp <Bpendragon@users.noreply.github.com>
2021-12-05 21:30:15 -0800

    2021 day 6

--- AdventOfCode/Solutions/Year2021/Day06-Solution.cs
+++ AdventOfCode/Solutions/Year2021/Day06-Solution.cs
@@ -14,19 +14,50 @@ namespace AdventOfCode.Solutions.Year2021
 
     class Day06 : ASolution
     {
-        public Day06() : base(06, 2021, "")
+        List<int> fishies;
+        long p1Answer;
+        long p2Answer;
+        public Day06() : base(06, 2021, "Lanternfish")
         {
+            fishies = Input.ToIntList();
+            Dictionary<int, long> fishCounts = new();
+            fishCounts[0] = fishies.Count(x => x == 0);
+            fishCounts[1] = fishies.Count(x => x == 1);
+            fishCounts[2] = fishies.Count(x => x == 2);
+            fishCounts[3] = fishies.Count(x => x == 3);
+            fishCounts[4] = fishies.Count(x => x == 4);
+            fishCounts[5] = fishies.Count(x => x == 5);
+            fishCounts[6] = fishies.Count(x => x == 6);
+            fishCounts[7] = fishies.Count(x => x == 7);
+            fishCounts[8] = fishies.Count(x => x == 8);
 
+            for (int i = 0; i < 256; i++)
+            {
+                long breeders = fishCounts[0];
+                fishCounts[0] = fishCounts[1];
+                fishCounts[1] = fishCounts[2];
+                fishCounts[2] = fishCounts[3];
+                fishCounts[3] = fishCounts[4];
+                fishCounts[4] = fishCounts[5];
+                fishCounts[5] = fishCounts[6];
+                fishCounts[6] = fishCounts[7] + breeders;
+                fishCounts[7] = fishCounts[8];
+                fishCounts[8] = breeders;
+
+                if (i == 79) p1Answer = fishCounts.Values.Sum();
+            }
+
+            p2Answer = fishCounts.Values.Sum();
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            return p1Answer.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            return p2Answer.ToString();
         }
     }
 }
