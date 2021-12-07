3acf0680f5c0da5fc3098f107f09998d4fd37b1e
David Camp <Bpendragon@users.noreply.github.com>
2021-12-06 21:22:01 -0800

    2021 day 7 AS SOLVED

--- AdventOfCode/Solutions/Year2021/Day07-Solution.cs
+++ AdventOfCode/Solutions/Year2021/Day07-Solution.cs
@@ -14,19 +14,44 @@ namespace AdventOfCode.Solutions.Year2021
 
     class Day07 : ASolution
     {
+        List<int> crabs;
         public Day07() : base(07, 2021, "")
         {
-
+            //UseDebugInput = true;
+            crabs = Input.ToIntList(",");
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            var minCrab = crabs.Min();
+            var maxCrab = crabs.Max();
+            long bestSoFar = long.MaxValue;
+            for(int i = minCrab; i <= maxCrab; i++)
+            {
+                var tmp = crabs.Sum(x => Math.Abs(x-i));
+                if (tmp < bestSoFar) bestSoFar = tmp;
+            }
+            return bestSoFar.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            var minCrab = crabs.Min();
+            var maxCrab = crabs.Max();
+            long bestSoFar = long.MaxValue;
+            Dictionary<int, long> costs = new();
+            costs[0] = 0;
+            costs[1] = 1;
+            for(int i = 2; i<=maxCrab;i++)
+            {
+                costs[i] = i + costs[i - 1];
+            }
+            for (int i = minCrab; i <= maxCrab; i++)
+            {
+                var tmp = crabs.Sum(x => costs[Math.Abs(x - i)]);
+                if (tmp < bestSoFar) bestSoFar = tmp;
+            }
+            return bestSoFar.ToString();
         }
     }
 }
