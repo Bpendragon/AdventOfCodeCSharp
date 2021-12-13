05592abac456e115657f8c800b4771ccb4bd010c
David Camp <Bpendragon@users.noreply.github.com>
2021-12-13 00:33:59 -0800

    2021 day 13 cleanup

--- AdventOfCode/Solutions/Year2021/Day13-Solution.cs
+++ AdventOfCode/Solutions/Year2021/Day13-Solution.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -11,51 +11,21 @@ namespace AdventOfCode.Solutions.Year2021
     {
         readonly Dictionary<Coordinate2D, string> paper = new();
         readonly List<string> folds = new();
+        readonly string part1;
         public Day13() : base(13, 2021, "Transparent Origami")
         {
             var halves = Input.Split("\n\n");
             foreach (var p in halves[0].SplitByNewline())
             {
                 var asInts = p.ToIntList(",");
-                paper[(asInts[0], asInts[1])] = "#";
+                paper[(asInts[0], asInts[1])] = "█";
             }
 
             folds = halves[1].SplitByNewline();
-        }
 
-        protected override string SolvePartOne()
-        {
-            var instruction = folds[0].Split()[2].Split("=");
-            int lineLoc = int.Parse(instruction[1]);
-            List<Coordinate2D> dotsToFlip = new();
-            if (instruction[0] == "x")
+            for(int i = 0; i < folds.Count; i++)
             {
-                dotsToFlip = paper.Keys.Where(a => a.x > lineLoc).ToList();
-                foreach(var dot in dotsToFlip)
-                {
-                    paper.Remove(dot);
-                    paper[(dot.x - ((dot.x - lineLoc) * 2), dot.y)] = "#";
-                }
-
-            }
-            else if (instruction[0] == "y")
-            {
-                dotsToFlip = paper.Keys.Where(a => a.y > lineLoc).ToList();
-                foreach (var dot in dotsToFlip)
-                {
-                    paper.Remove(dot);
-                    paper[(dot.x, dot.y - ((dot.y - lineLoc) * 2))] = "#";
-                }
-            }
-            else throw new ArgumentException();
-            return paper.Count.ToString();
-        }
-
-        protected override string SolvePartTwo()
-        {
-            foreach(var tmp in folds.Skip(1))
-            {
-                var instruction = tmp.Split()[2].Split("=");
+                var instruction = folds[i].Split()[2].Split("=");
                 int lineLoc = int.Parse(instruction[1]);
                 List<Coordinate2D> dotsToFlip = new();
                 if (instruction[0] == "x")
@@ -65,9 +35,8 @@ namespace AdventOfCode.Solutions.Year2021
                     {
                         paper.Remove(dot);
                         int newXCoord = dot.x - ((dot.x - lineLoc) * 2);
-                        paper[(newXCoord, dot.y)] = "#";
+                        paper[(newXCoord, dot.y)] = "█";
                     }
-
                 }
                 else if (instruction[0] == "y")
                 {
@@ -76,17 +45,34 @@ namespace AdventOfCode.Solutions.Year2021
                     {
                         paper.Remove(dot);
                         int newYCoord = dot.y - ((dot.y - lineLoc) * 2);
-                        paper[(dot.x, newYCoord)] = "#";
+                        paper[(dot.x, newYCoord)] = "█";
                     }
                 }
                 else throw new ArgumentException();
+
+                if (i == 0) part1 = paper.Count.ToString();
             }
+
+        }
+
+        protected override string SolvePartOne()
+        {
+            return part1;
+        }
+
+        protected override string SolvePartTwo()
+        {
+            return PrintStep();
+        }
+
+        private string PrintStep()
+        {
             int maxX = paper.Max(a => a.Key.x);
             int maxY = paper.Max(a => a.Key.y);
             StringBuilder sb = new("\n");
             for (int i = 0; i <= maxY; i++)
             {
-                for(int j = 0; j <= maxX; j++)
+                for (int j = 0; j <= maxX; j++)
                 {
                     sb.Append(paper.GetValueOrDefault((j, i), " "));
                 }
