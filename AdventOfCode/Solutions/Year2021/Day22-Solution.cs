ba4e70b709a72c93315b6839f5b0a706ac822b84
David Camp <Bpendragon@users.noreply.github.com>
2021-12-22 12:21:04 -0800

    2021 day 21 FAST BOI version (~85ms TOTAL runtime)

--- AdventOfCode/Solutions/Year2021/Day22-Solution.cs
+++ AdventOfCode/Solutions/Year2021/Day22-Solution.cs
@@ -9,44 +9,35 @@ namespace AdventOfCode.Solutions.Year2021
 
     class Day22 : ASolution
     {
-        List<(bool turnOn, int minX, int maxX, int minY, int maxY, int minZ, int maxZ)> steps = new();
+        string part1 = string.Empty;
+        string part2 = string.Empty;
         public Day22() : base(22, 2021, "Reactor Reboot")
         {
+            Dictionary<(int minX, int maxX, int minY, int maxY, int minZ, int maxZ), long> cubes = new();
+
+            int lineNum = 1;
             foreach(var line in Input.SplitByNewline())
             {
                 bool turnOn = line[..2] == "on";
                 var rawList = Regex.Split(line, @"[^\d-]+");
                 var intList = rawList.Where(s => !string.IsNullOrWhiteSpace(s)).ToList();
-                steps.Add((turnOn, int.Parse(intList[0]), int.Parse(intList[1]), int.Parse(intList[2]), int.Parse(intList[3]), int.Parse(intList[4]), int.Parse(intList[5])));
-            }
-        }
 
-        protected override string SolvePartOne()
-        {
-            Dictionary<Coordinate3D, bool> firstMap = new();
-            foreach(var s in steps.Take(20))
-            {
-                firstMap = RunStep(s, firstMap);
-            }
-            return firstMap.Count(a => a.Value).ToString();
-        }
-
-        protected override string SolvePartTwo()
-        {
-            Dictionary<(int minX, int maxX, int minY, int maxY, int minZ, int maxZ), long> cubes = new();
-
-            foreach(var step in steps)
-            {
-                (bool turnOn, int minX, int maxX, int minY, int maxY, int minZ, int maxZ) = step;
+                (int minX, int maxX, int minY, int maxY, int minZ, int maxZ) = (int.Parse(intList[0]), int.Parse(intList[1]), int.Parse(intList[2]), int.Parse(intList[3]), int.Parse(intList[4]), int.Parse(intList[5]));
                 long newSign = turnOn ? 1 : -1;
                 var newCuboid = (minX, maxX, minY, maxY, minZ, maxZ);
 
                 Dictionary<(int minX, int maxX, int minY, int maxY, int minZ, int maxZ), long> newCuboids = new();
 
-                foreach(var kvp in cubes)
+                foreach (var kvp in cubes.ToList())
                 {
                     (int minX2, int maxX2, int minY2, int maxY2, int minZ2, int maxZ2) = kvp.Key;
                     var curSign = kvp.Value;
+
+                    if (curSign == 0)
+                    {
+                        cubes.Remove(kvp.Key);
+                        continue;
+                    }
 
                     //These determine the overlapping region, it even grabs complete interior bits.
                     int tmpMinX = Math.Max(minX, minX2);
@@ -68,28 +59,27 @@ namespace AdventOfCode.Solutions.Year2021
                 if (newSign == 1) newCuboids[newCuboid] = newCuboids.GetValueOrDefault(newCuboid, 0) + newSign;
 
                 //Add or update the value of the cuboids.
-                foreach(var kvp in newCuboids)
+                foreach (var kvp in newCuboids)
                 {
                     cubes[kvp.Key] = cubes.GetValueOrDefault(kvp.Key, 0) + kvp.Value;
                 }
+
+                if (lineNum == 20) part1 = cubes.Sum(a => (a.Key.maxX - a.Key.minX + 1L) * (a.Key.maxY - a.Key.minY + 1) * (a.Key.maxZ - a.Key.minZ + 1) * a.Value).ToString();
+                lineNum++;
             }
-            return cubes.Sum(a => (a.Key.maxX - a.Key.minX + 1L) * (a.Key.maxY - a.Key.minY + 1) * (a.Key.maxZ - a.Key.minZ + 1) * a.Value).ToString();
+
+            part2 = cubes.Sum(a => (a.Key.maxX - a.Key.minX + 1L) * (a.Key.maxY - a.Key.minY + 1) * (a.Key.maxZ - a.Key.minZ + 1) * a.Value).ToString();
         }
 
-        private static Dictionary<Coordinate3D, bool> RunStep((bool turnOn, int minX, int maxX, int minY, int maxY, int minZ, int maxZ) step, Dictionary<Coordinate3D, bool> currentMap)
+        protected override string SolvePartOne()
         {
-            var (turnOn, minX, maxX, minY, maxY, minZ, maxZ) = step;
-            foreach (int x in Enumerable.Range(minX, (maxX-minX) + 1))
-            {
-                foreach(int y in Enumerable.Range(minY, (maxY-minY)+1))
-                {
-                    foreach(int z in Enumerable.Range(minZ, (maxZ-minZ)+1))
-                    {
-                        currentMap[(x, y, z)] = turnOn;
-                    }
-                }
-            }
-            return currentMap;
+            return part1;
+        }
+
+        protected override string SolvePartTwo()
+        {
+
+            return part2;
         }
     }
 }
