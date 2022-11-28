6da65e6d21b6118950a40739203f8b8a745c2a6d
David Camp <Bpendragon@users.noreply.github.com>
2022-11-27 20:56:22 -0800

    Pre-generate Solution Files

---
+++ AdventOfCode/Solutions/Year2022/Day06-Solution.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using AdventOfCode.UserClasses;
+using System.Linq;
+using System.Data;
+using System.Threading;
+using System.Security;
+using static AdventOfCode.Solutions.Utilities;
+using System.Runtime.CompilerServices;
+
+namespace AdventOfCode.Solutions.Year2022
+{
+
+    class Day06 : ASolution
+    {
+        public Day06() : base(06, 2022, "")
+        {
+
+        }
+
+        protected override string SolvePartOne()
+        {
+            return null;
+        }
+
+        protected override string SolvePartTwo()
+        {
+            return null;
+        }
+    }
+}
---
+++ AdventOfCode/Solutions/Year2022/Day21-Solution.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using AdventOfCode.UserClasses;
+using System.Linq;
+using System.Data;
+using System.Threading;
+using System.Security;
+using static AdventOfCode.Solutions.Utilities;
+using System.Runtime.CompilerServices;
+
+namespace AdventOfCode.Solutions.Year2022
+{
+
+    class Day21 : ASolution
+    {
+        public Day21() : base(21, 2022, "")
+        {
+
+        }
+
+        protected override string SolvePartOne()
+        {
+            return null;
+        }
+
+        protected override string SolvePartTwo()
+        {
+            return null;
+        }
+    }
+}
