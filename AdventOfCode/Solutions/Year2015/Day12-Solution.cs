873fc5517296aea597bf3a996f1125a7ec02a94f
David Camp <Bpendragon@users.noreply.github.com>
2022-12-07 21:47:45 -0800

    2022 Day 8

--- AdventOfCode/Solutions/Year2015/Day12-Solution.cs
+++ AdventOfCode/Solutions/Year2015/Day12-Solution.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Linq;
 using System.Text.RegularExpressions;
-
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
--- AdventOfCode/Solutions/Year2022/Day08-Solution.cs
+++ AdventOfCode/Solutions/Year2022/Day08-Solution.cs
@@ -1,33 +1,180 @@
-using System;
-using System.Text;
 using System.Collections.Generic;
-using AdventOfCode.UserClasses;
-using System.Linq;
-using System.Data;
-using System.Threading;
-using System.Security;
 using static AdventOfCode.Solutions.Utilities;
-using System.Runtime.CompilerServices;
 
 namespace AdventOfCode.Solutions.Year2022
 {
 
-    [DayInfo(08, 2022, "")]
+    [DayInfo(08, 2022, "Treetop Tree House")]
     class Day08 : ASolution
     {
+        readonly Dictionary<Coordinate2D, int> trees = new();
+        readonly int maxX, maxY;
         public Day08() : base()
         {
-
+            var lines = Input.SplitByNewline();
+            maxY = lines.Count - 1;
+            maxX = lines[0].Length - 1;
+            for (int y = 0; y < lines.Count; y++)
+            {
+                var nums = lines[y].ToIntList();
+                for (int x = 0; x < nums.Count; x++)
+                {
+                    trees[(x, y)] = nums[x];
+                }
+            }
         }
 
         protected override object SolvePartOne()
         {
-            return null;
+            HashSet<Coordinate2D> visibleTrees = new();
+
+            for (int y = 0; y <= maxY; y++)
+            {
+                int tallest;
+                Coordinate2D next;
+                if (y == 0 || y == maxY) //top and bottom edges
+                {
+
+                    for (int x = 0; x <= maxX; x++)
+                    {
+                        tallest = trees[(x, y)];
+                        visibleTrees.Add((x, y));
+                        //Search North
+                        next = (x, y).MoveDirection(CompassDirection.N);
+                        while (trees.TryGetValue(next, out int height))
+                        {
+                            if (height > tallest)
+                            {
+                                tallest = height;
+                                visibleTrees.Add(next);
+                            }
+                            next = next.MoveDirection(CompassDirection.N);
+                        }
+                        //Search South
+                        tallest = trees[(x, y)];
+                        next = (x, y).MoveDirection(CompassDirection.S);
+                        while (trees.TryGetValue(next, out int height))
+                        {
+                            if (height > tallest)
+                            {
+                                tallest = height;
+                                visibleTrees.Add(next);
+                            }
+                            next = next.MoveDirection(CompassDirection.S);
+                        }
+                    }
+                }
+                else // vertical edges
+                {
+                    //Search East
+                    tallest = trees[(0, y)];
+                    visibleTrees.Add((0, y));
+                    next = (0, y).MoveDirection(CompassDirection.E);
+                    while (trees.TryGetValue(next, out int height))
+                    {
+                        if (height > tallest)
+                        {
+                            tallest = height;
+                            visibleTrees.Add(next);
+                        }
+                        next = next.MoveDirection(CompassDirection.E);
+                    }
+
+                    //Search West
+                    tallest = trees[(maxX, y)];
+                    visibleTrees.Add((maxX, y));
+                    next = (maxX, y).MoveDirection(CompassDirection.W);
+                    while (trees.TryGetValue(next, out int height))
+                    {
+                        if (height > tallest)
+                        {
+                            tallest = height;
+                            visibleTrees.Add(next);
+                        }
+                        next = next.MoveDirection(CompassDirection.W);
+                    }
+                }
+            }
+
+            return visibleTrees.Count;
         }
 
         protected override object SolvePartTwo()
         {
-            return null;
+            int maxScore = 0;
+            for (int y = 0; y <= maxY; y++)
+            {
+                for (int x = 0; x <= maxX; x++)
+                {
+                    int maxAllowed = trees[(x, y)];
+                    int scenicScore = 1;
+                    int visTrees = 0;
+                    Coordinate2D next = (x, y).MoveDirection(CompassDirection.N);
+                    //North
+                    while(trees.TryGetValue(next, out int height))
+                    {
+                        if (height < maxAllowed) visTrees++;
+                        if(height == maxAllowed)
+                        {
+                            visTrees++;
+                            break;
+                        }
+                        next = next.MoveDirection(CompassDirection.N);
+                    }
+                    scenicScore *= visTrees;
+                    if (scenicScore == 0) continue;
+                    visTrees = 0;
+                    //South
+                    next = (x, y).MoveDirection(CompassDirection.S);
+                    while (trees.TryGetValue(next, out int height))
+                    {
+                        if (height < maxAllowed) visTrees++;
+                        if (height >= maxAllowed)
+                        {
+                            visTrees++;
+                            break;
+                        }
+                        next = next.MoveDirection(CompassDirection.S);
+                    }
+                    scenicScore *= visTrees;
+                    if (scenicScore == 0) continue;
+                    visTrees = 0;
+
+                    //East
+                    next = (x, y).MoveDirection(CompassDirection.E);
+                    while (trees.TryGetValue(next, out int height))
+                    {
+                        if (height < maxAllowed) visTrees++;
+                        if (height == maxAllowed)
+                        {
+                            visTrees++;
+                            break;
+                        }
+                        next = next.MoveDirection(CompassDirection.E);
+                    }
+                    scenicScore *= visTrees;
+                    if (scenicScore == 0) continue;
+                    visTrees = 0;
+
+                    //West
+                    next = (x, y).MoveDirection(CompassDirection.W);
+                    while (trees.TryGetValue(next, out int height))
+                    {
+                        if (height < maxAllowed) visTrees++;
+                        if (height == maxAllowed)
+                        {
+                            visTrees++;
+                            break;
+                        }
+                        next = next.MoveDirection(CompassDirection.W);
+                    }
+                    scenicScore *= visTrees;
+                    if (scenicScore > maxScore) maxScore = scenicScore;
+
+                }
+            }
+
+            return maxScore;
         }
     }
 }
