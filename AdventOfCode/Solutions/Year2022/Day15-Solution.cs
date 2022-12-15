5c84fbd94145e22f02dcc0efb595bb51e43a2e43
David Camp <Bpendragon@users.noreply.github.com>
2022-12-14 23:15:53 -0800

    2022 Day 15 cleanuo

--- AdventOfCode/Solutions/Year2022/Day15-Solution.cs
+++ AdventOfCode/Solutions/Year2022/Day15-Solution.cs
@@ -1,18 +1,14 @@
 using System;
-using System.Text;
 using System.Collections.Generic;
-using AdventOfCode.UserClasses;
-using System.Linq;
 using System.Data;
-using System.Threading;
-using System.Security;
+using System.Linq;
+
 using static AdventOfCode.Solutions.Utilities;
-using System.Runtime.CompilerServices;
 
 namespace AdventOfCode.Solutions.Year2022
 {
 
-    [DayInfo(15, 2022, "")]
+    [DayInfo(15, 2022, "Beacon Exclusion Zone")]
     class Day15 : ASolution
     {
         Dictionary<Coordinate2D, char> map = new();
