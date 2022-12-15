440834f077f3ba866ffb7b673725d6fd3efa80a7
David Camp <Bpendragon@users.noreply.github.com>
2022-12-14 23:11:11 -0800

    2022 Day 15

--- AdventOfCode/Solutions/Year2022/Day15-Solution.cs
+++ AdventOfCode/Solutions/Year2022/Day15-Solution.cs
@@ -15,19 +15,108 @@ namespace AdventOfCode.Solutions.Year2022
     [DayInfo(15, 2022, "")]
     class Day15 : ASolution
     {
+        Dictionary<Coordinate2D, char> map = new();
+        Dictionary<Coordinate2D, int> Sensors = new();
         public Day15() : base()
         {
+            foreach(var line in Input.SplitByNewline())
+            {
+                var a = line.ExtractInts().ToArray();
+                Coordinate2D sensor = (a[0], a[1]);
+                Coordinate2D beacon = (a[2], a[3]);
 
+                map[sensor] = 'S';
+                map[beacon] = 'B';
+                Sensors[sensor] = sensor.ManDistance(beacon);
+
+            }
         }
 
         protected override object SolvePartOne()
         {
-            return null;
+            int yToCheck;
+            if (UseDebugInput) yToCheck = 10;
+            else yToCheck = 2_000_000;
+            HashSet<Coordinate2D> points = new();
+            foreach(var s in Sensors)
+            {
+                var sensor = s.Key;
+                var maxDist = s.Value;
+                var vertDiff = sensor.ManDistance((sensor.x, yToCheck));
+
+
+                if ( vertDiff > maxDist) continue;
+
+                for(int x = sensor.x - (maxDist - vertDiff); x <= sensor.x + (maxDist - vertDiff); x++)
+                {
+                    if (map.TryGetValue((x, yToCheck), out char v) && v == 'B') continue;
+                    points.Add((x, yToCheck));
+                }
+
+            }
+
+            return points.Count;
         }
 
         protected override object SolvePartTwo()
         {
-            return null;
+            Coordinate2D beaconSpot = (-1, -1);
+            
+
+            var sensorsAsList = Sensors.Keys.ToArray();
+            for (int i = 0; i < sensorsAsList.Length; i++)
+            {
+                var curSens = sensorsAsList[i];
+                if (TestDir(CompassDirection.SE, curSens, out beaconSpot)) break;
+                if (TestDir(CompassDirection.NE, curSens, out beaconSpot)) break;
+                if (TestDir(CompassDirection.SW, curSens, out beaconSpot)) break;
+                if (TestDir(CompassDirection.SE, curSens, out beaconSpot)) break;
+            }
+
+            return ((long)beaconSpot.x * 4000000) + beaconSpot.y;
+
+
+        }
+
+        bool TestDir(CompassDirection dir, Coordinate2D sensorLoc, out Coordinate2D beaconSpot)
+        {
+            var nearbySensors = Sensors.Keys.Where(a => a != sensorLoc && sensorLoc.ManDistance(a) < Sensors[sensorLoc] + Sensors[a] + 2);
+            long maxX, maxY;
+            if (UseDebugInput) maxX = maxY = 20;
+            else maxX = maxY = 4_000_000;
+
+
+            Coordinate2D curLoc = (dir) switch
+            {
+                CompassDirection.SW => sensorLoc.MoveDirection(CompassDirection.N, false, Sensors[sensorLoc] + 1),
+                CompassDirection.SE => sensorLoc.MoveDirection(CompassDirection.N, false, Sensors[sensorLoc] + 1),
+                CompassDirection.NE => sensorLoc.MoveDirection(CompassDirection.S, false, Sensors[sensorLoc] + 1),
+                CompassDirection.NW => sensorLoc.MoveDirection(CompassDirection.S, false, Sensors[sensorLoc] + 1),
+                _ => throw new ArgumentException("Only diagonals this time bucko")
+            };
+
+
+            if (nearbySensors.All(a => Sensors[a] < a.ManDistance(curLoc)) && !(curLoc.x <= 0 || curLoc.x >= maxX || curLoc.y <= 0 || curLoc.y >= maxY))
+            {
+                beaconSpot = curLoc;
+                return true;
+            }
+
+            while (curLoc.y != sensorLoc.y)
+            {
+                curLoc = curLoc.MoveDirection(dir);
+
+                if (curLoc.x <= 0 || curLoc.x >= maxX || curLoc.y <= 0 || curLoc.y >= maxY) continue;
+
+                if (nearbySensors.All(a => Sensors[a] < a.ManDistance(curLoc)))
+                {
+                    beaconSpot = curLoc;
+                    return true;
+                }
+            }
+
+            beaconSpot = (-1, -1);
+            return false;
         }
     }
 }
