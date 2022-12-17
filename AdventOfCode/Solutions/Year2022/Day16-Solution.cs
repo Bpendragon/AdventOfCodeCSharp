169a4fe7e055f96a00a0a69e91ca7a2d05a5f4b8
David Camp <Bpendragon@users.noreply.github.com>
2022-12-16 18:25:04 -0800

    Initioal musings

--- AdventOfCode/Solutions/Year2022/Day16-Solution.cs
+++ AdventOfCode/Solutions/Year2022/Day16-Solution.cs
@@ -8,6 +8,7 @@ using System.Threading;
 using System.Security;
 using static AdventOfCode.Solutions.Utilities;
 using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Solutions.Year2022
 {
@@ -15,19 +16,118 @@ namespace AdventOfCode.Solutions.Year2022
     [DayInfo(16, 2022, "")]
     class Day16 : ASolution
     {
-        public Day16() : base()
+        Dictionary<string, Valve> Valves = new();
+        int[,] dists;
+        readonly List<string> ValveList;
+
+        public Day16() : base(true)
         {
+            foreach (var l in Input.SplitByNewline())
+            {
+                var c = Regex.Matches(l, "([A-Z]{2}|\\d+)").ToList();
+
+                Valve newValve = new()
+                {
+                    Name = c[0].Value,
+                    Flowrate = int.Parse(c[1].Value),
+                    Neighbors = new(c.Skip(2).Select(a => a.Value)),
+                };
+                Valves[c[0].Value] = newValve;
+                foreach (var n in newValve.Neighbors)
+                {
+                    newValve.ValveDists[n] = 1;
+                }
+            }
+
+            //Floyd Warshall???? (I think)
+
+            ValveList = Valves.Values.OrderBy(a => a.Name).Select(a => a.Name).ToList();
+
+            dists = new int[Valves.Count, Valves.Count];
+            for (int i = 0; i < ValveList.Count; i++) //Fill in the default values
+            {
+                for (int j = i; j < ValveList.Count; j++)
+                {
+                    if (i == j) dists[i, j] = 0;
+                    else if (Valves[ValveList[i]].Neighbors.Contains(ValveList[j]))
+                    {
+                        dists[i, j] = dists[j, i] = 1;
+                    }
+                    else
+                    {
+                        dists[i, j] = dists[j, i] = 9999; //Don't use int.MaxValue here because we need to do some additions.
+                    }
+                }
+            }
+
+            for (int k = 0; k < ValveList.Count; k++)
+            {
+                for (int i = 0; i < ValveList.Count; i++)
+                {
+                    for (int j = i + 1; j < ValveList.Count; j++)
+                    {
+                        if (dists[i, k] + dists[k, j] < dists[i, j])
+                            dists[i, j] = dists[j, i] = dists[i, k] + dists[k, j];
+                    }
+                }
+            }
 
         }
 
         protected override object SolvePartOne()
         {
-            return null;
+            string current = "AA";
+            int totalFlow = 0;
+            HashSet<string> openValves = new();
+
+
+
+            return SearchSubtree("AA", null, openValves, 0, 31);
         }
 
         protected override object SolvePartTwo()
         {
             return null;
+        }
+
+        class Valve
+        {
+            public string Name { get; set; }
+            public int Flowrate { get; set; }
+            public Dictionary<string, int> ValveDists { get; set; } = new();
+            public List<string> Neighbors { get; set; }
+        }
+
+        public int SearchSubtree(string nextValve, string prevValve, HashSet<string> openValves, int curRelief, int TimeRemaining) //prevValve allows us to backtrack from the end of a long branch
+        {
+            if (TimeRemaining < 2) return curRelief;
+            int newRelief = 0;
+            TimeRemaining--; //Walk to next valve
+
+            var curValve = Valves[nextValve];
+
+            if (openValves.Add(nextValve) && curValve.Flowrate != 0)
+            {
+                TimeRemaining--; //open the valve
+                newRelief += TimeRemaining * curValve.Flowrate;
+            }
+
+            if (openValves.Count == Valves.Keys.Count) return newRelief; //We've visited every valve, all we can do is wait.
+
+            if (curValve.Neighbors.Count == 1)
+            {
+                return newRelief + SearchSubtree(curValve.Neighbors.SingleOrDefault(), curValve.Name, new(openValves), newRelief, TimeRemaining);
+            }
+
+            int t = 0;
+            foreach (var v in curValve.Neighbors.Where(a => a != prevValve))
+            {
+                var s = SearchSubtree(v, curValve.Name, new(openValves), newRelief, TimeRemaining);
+                if (s > t) t = s;
+            }
+
+
+            return newRelief + t;
         }
     }
 }
