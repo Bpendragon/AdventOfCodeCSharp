48bd274b578c8c0f4b8f498a9ce54d591dff0dd5
David Camp <Bpendragon@users.noreply.github.com>
2022-12-24 21:42:20 -0800

    2022 Day 25 ❄️🎄Happy Advent of Code🎄❄️

--- AdventOfCode/Solutions/Year2022/Day25-Solution.cs
+++ AdventOfCode/Solutions/Year2022/Day25-Solution.cs
@@ -1,13 +1,5 @@
-using System;
-using System.Text;
+﻿using System;
 using System.Collections.Generic;
-using AdventOfCode.UserClasses;
-using System.Linq;
-using System.Data;
-using System.Threading;
-using System.Security;
-using static AdventOfCode.Solutions.Utilities;
-using System.Runtime.CompilerServices;
 
 namespace AdventOfCode.Solutions.Year2022
 {
@@ -15,19 +7,73 @@ namespace AdventOfCode.Solutions.Year2022
     [DayInfo(25, 2022, "")]
     class Day25 : ASolution
     {
+        Dictionary<char, long> SnafuDigits = new()
+        {
+            { '0', 0 },
+            { '1', 1 },
+            { '2', 2 },
+            { '-', -1 },
+            { '=', -2 },
+        };
+        List<string> SnafusLE = new();
         public Day25() : base()
         {
-
+            foreach(var l in Input.SplitByNewline())
+            {
+                SnafusLE.Add(l.Reverse());
+            }
         }
 
         protected override object SolvePartOne()
         {
-            return null;
+            long runningSum = 0;
+            foreach(var s in SnafusLE)
+            {
+                long tmp = 0;
+                long pwr = 1;
+                foreach(char c in s)
+                {
+                    tmp += c switch
+                    {
+                        '1' => pwr,
+                        '2' => 2 * pwr,
+                        '0' => 0,
+                        '-' => -pwr,
+                        '=' => -2 * pwr
+                    };
+
+                    pwr *= 5;
+                }
+
+                runningSum += tmp;
+            }
+
+            Console.WriteLine(runningSum);
+
+            return ToSnafu(runningSum);
         }
 
         protected override object SolvePartTwo()
         {
-            return null;
+            return "❄️🎄Happy Advent of Code🎄❄️";
+        }
+
+        string ToSnafu(long num)
+        {
+            if (num == 0) return "";
+
+            var remainder = num % 5;
+            foreach(var kvp in SnafuDigits)
+            {
+                var (k, v) = kvp;
+                if((v + 5) % 5 == remainder)
+                {
+                    long nxt = (num - v) / 5;
+                    return ToSnafu(nxt) + k;
+                }
+            }
+
+            return "";
         }
     }
 }
