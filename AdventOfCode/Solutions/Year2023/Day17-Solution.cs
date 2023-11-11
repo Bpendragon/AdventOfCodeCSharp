17e3d970b4f4e0f7207f2977654e657edf17627e
David Camp <Bpendragon@users.noreply.github.com>
2023-11-11 00:04:35 -0800

    Add files for 2023

---
+++ AdventOfCode/Solutions/Year2023/Day17-Solution.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using AdventOfCode.UserClasses;
+using System.Linq;
+using System.Data;
+using System.Threading;
+using System.Security;
+using static AdventOfCode.Solutions.Utilities;
+using System.Runtime.CompilerServices;
+
+namespace AdventOfCode.Solutions.Year2023
+{
+    [DayInfo(17, 2023, "")]
+    class Day17 : ASolution
+    {
+        public Day17() : base()
+        {
+
+        }
+
+        protected override object SolvePartOne()
+        {
+            return null;
+        }
+
+        protected override object SolvePartTwo()
+        {
+            return null;
+        }
+    }
+}
