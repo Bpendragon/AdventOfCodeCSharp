02562f22e0c1eca1cb169610901a8ef57d174cc7
David Camp <Bpendragon@users.noreply.github.com>
2023-11-30 21:35:38 -0800

    2023 day 1, the ugly way

--- AdventOfCode/Solutions/Year2023/Day01-Solution.cs
+++ AdventOfCode/Solutions/Year2023/Day01-Solution.cs
@@ -1,32 +1,75 @@
-using System;
-using System.Text;
 using System.Collections.Generic;
-using AdventOfCode.UserClasses;
-using System.Linq;
 using System.Data;
-using System.Threading;
-using System.Security;
-using static AdventOfCode.Solutions.Utilities;
-using System.Runtime.CompilerServices;
+using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2023
 {
-    [DayInfo(01, 2023, "")]
+    [DayInfo(01, 2023, "Trebuchet?!")]
     class Day01 : ASolution
     {
+        readonly List<string> Lines = [];
         public Day01() : base()
         {
-
+            Lines = Input.SplitByNewline();
         }
 
         protected override object SolvePartOne()
         {
-            return null;
+            int sum = 0;
+
+            foreach(var l in Lines)
+            {
+                var nums = l.ToCharArray().Where(char.IsDigit).ToArray();
+                if (int.TryParse($"{nums[0]}{nums[^1]}", out int val))
+                {
+                    sum += val;
+                }
+            }
+            return sum;
         }
 
         protected override object SolvePartTwo()
         {
-            return null;
+            string[] nums = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" }; //Yes I know zero isn't used, I need the index offset
+            int sum = 0;
+
+            foreach(var l in Lines)
+            {
+                int first = 10;
+                int last = 1;
+                bool firstFound = false;
+
+                for(int i = 0; i < l.Length; i++)
+                {
+                    var tstString = l[i..];
+                    if (char.IsDigit(tstString[0]))
+                    {
+                        var val = int.Parse(tstString[0].ToString());
+                        if (!firstFound)
+                        {
+                            firstFound = true;
+                            first *= val;
+                        }
+                        last = val;
+                        continue;
+                    }
+                    for(int j = 1; j < 10; j++)
+                    {
+                        if (tstString.StartsWith(nums[j]))
+                        {
+                            if (!firstFound)
+                            {
+                                firstFound = true;
+                                first *= j;
+                            }
+                            last = j;
+                        }
+                    }
+                }
+
+                sum += first + last;
+            }
+            return sum;
         }
     }
 }
