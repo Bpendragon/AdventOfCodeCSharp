d57dc20fdf6ad8436add009fb04e5d2e91b1cd28
David Camp <Bpendragon@users.noreply.github.com>
2023-12-08 21:41:35 -0800

    2023 Day 9

--- AdventOfCode/Solutions/Year2023/Day09-Solution.cs
+++ AdventOfCode/Solutions/Year2023/Day09-Solution.cs
@@ -1,32 +1,57 @@
-using System;
-using System.Text;
 using System.Collections.Generic;
-using AdventOfCode.UserClasses;
 using System.Linq;
-using System.Data;
-using System.Threading;
-using System.Security;
-using static AdventOfCode.Solutions.Utilities;
-using System.Runtime.CompilerServices;
 
 namespace AdventOfCode.Solutions.Year2023
 {
-    [DayInfo(09, 2023, "")]
+    [DayInfo(09, 2023, "Mirage Maintenance")]
     class Day09 : ASolution
     {
+        long part1 = 0;
+        long part2 = 0;
+
         public Day09() : base()
         {
+            foreach (var l in Input.SplitByNewline())
+            {
+                List<long> r = l.ExtractLongs().ToList();
+                List<List<long>> curReduction = new();
+                curReduction.Add(r);
+
+                while (curReduction.Last().Any(a => a != 0))
+                {
+                    var curList = curReduction.Last();
+                    List<long> nextReduction = new(curList.Count - 1); //Preallocate memory to save a few nanoseconds
+                    for (int i = 1; i < curList.Count; i++)
+                    {
+                        nextReduction.Add(curList[i] - curList[i - 1]);
+                    }
+                    curReduction.Add(nextReduction);
+                }
+
+                curReduction.Reverse();
+
+                long p1Val = 0;
+                long p2Val = 0;
+                foreach (var a in curReduction.Skip(1))
+                {
+                    p1Val += a.Last();
+                    p2Val =  a.First() - p2Val;
+                }
+
+                part1 += p1Val;
+                part2 += p2Val;
+            }
 
         }
 
         protected override object SolvePartOne()
         {
-            return null;
+            return part1;
         }
 
         protected override object SolvePartTwo()
         {
-            return null;
+            return part2;
         }
     }
 }
