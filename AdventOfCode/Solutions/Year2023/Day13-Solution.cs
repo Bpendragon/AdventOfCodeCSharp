57512b3a3a91b6a8c9d1e09d2c3f00e379c0c46a
David Camp <Bpendragon@users.noreply.github.com>
2023-12-12 21:53:47 -0800

    2023 Day 13

--- AdventOfCode/Solutions/Year2023/Day13-Solution.cs
+++ AdventOfCode/Solutions/Year2023/Day13-Solution.cs
@@ -1,32 +1,96 @@
 using System;
-using System.Text;
 using System.Collections.Generic;
-using AdventOfCode.UserClasses;
 using System.Linq;
-using System.Data;
-using System.Threading;
-using System.Security;
+using System.Text;
+
 using static AdventOfCode.Solutions.Utilities;
-using System.Runtime.CompilerServices;
 
 namespace AdventOfCode.Solutions.Year2023
 {
-    [DayInfo(13, 2023, "")]
+    [DayInfo(13, 2023, "Point of Incidence")]
     class Day13 : ASolution
     {
+        List<string> maps = new();
+        Dictionary<int, (int num, bool isRow)> initialMirrors= new(); 
         public Day13() : base()
         {
-
+            maps = Input.SplitByDoubleNewline();
         }
 
         protected override object SolvePartOne()
         {
-            return null;
+            int sum = 0;
+            for (int i = 0; i < maps.Count; i++)
+            {
+                string block = maps[i];
+                TryFindReflection(block, i, out int res);
+                sum += res;
+            }
+
+            return sum;
         }
 
         protected override object SolvePartTwo()
         {
-            return null;
+            int sum = 0;
+            for (int j = 0; j < maps.Count; j++)
+            {
+                string block = maps[j];
+                for (int i = 0; i < block.Length; i++)
+                {
+                    if (!".#".Contains(block[i])) continue;
+                    StringBuilder sb = new StringBuilder(block);
+
+                    sb[i] = sb[i] == '.' ? '#' : '.';
+                    int res = 0;
+                    if(TryFindReflection(sb.ToString(), j, out res))
+                    {
+                        sum += res;
+                        break;
+                    }
+                    
+                }
+            }
+
+            return sum;
+        }
+
+        private bool TryFindReflection(string block, int Id, out int result)
+        {
+            var asRows = block.SplitByNewline();
+            var asColumns = block.SplitIntoColumns().ToList();
+            //Check rows
+            for (int i = 1; i < asRows.Count; i++)
+            {
+                if (asRows.Take(i).Reverse().Zip(asRows.Skip(i)).All(x => x.First == x.Second))
+                {
+                    if(initialMirrors.TryGetValue(Id, out (int num, bool isRow) x))
+                    {
+                        if (x.isRow && x.num == i) continue;
+                    }
+                    initialMirrors[Id] = (i, true); 
+                    result = i * 100;
+                    return true;
+                }
+            }
+
+            for (int i = 1; i < asColumns.Count; i++)
+            {
+                if (asColumns.Take(i).Reverse().Zip(asColumns.Skip(i)).All(x => x.First == x.Second))
+                {
+                    if (initialMirrors.TryGetValue(Id, out (int num, bool isRow) x))
+                    {
+                        if (!x.isRow && x.num == i) continue;
+                        
+                    }
+                    initialMirrors[Id] = (i, false);
+                    result = i;
+                    return true;
+                }
+            }
+
+            result = 0;
+            return false;
         }
     }
 }
