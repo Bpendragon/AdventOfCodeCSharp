be30b9e79eb88303aabcc037b3d45a1435886f80
Bpendragon <david@campclan.net>
2024-03-22 18:44:46 -0700

    Preemptively generate solutions for 2024

---
+++ AdventOfCode/Solutions/Year2024/Day04-Solution.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using AdventOfCode.UserClasses;
+using System.Linq;
+using System.Data;
+using System.Threading;
+using System.Security;
+using static AdventOfCode.Solutions.Utilities;
+using System.Runtime.CompilerServices;
+
+namespace AdventOfCode.Solutions.Year2024
+{
+    [DayInfo(04, 2024, "")]
+    class Day04 : ASolution
+    {
+        public Day04() : base()
+        {
+
+        }
+
+        protected override object SolvePartOne()
+        {
+            return null;
+        }
+
+        protected override object SolvePartTwo()
+        {
+            return null;
+        }
+    }
+}
---
+++ AdventOfCode/Solutions/Year2024/Day13-Solution.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using AdventOfCode.UserClasses;
+using System.Linq;
+using System.Data;
+using System.Threading;
+using System.Security;
+using static AdventOfCode.Solutions.Utilities;
+using System.Runtime.CompilerServices;
+
+namespace AdventOfCode.Solutions.Year2024
+{
+    [DayInfo(13, 2024, "")]
+    class Day13 : ASolution
+    {
+        public Day13() : base()
+        {
+
+        }
+
+        protected override object SolvePartOne()
+        {
+            return null;
+        }
+
+        protected override object SolvePartTwo()
+        {
+            return null;
+        }
+    }
+}
