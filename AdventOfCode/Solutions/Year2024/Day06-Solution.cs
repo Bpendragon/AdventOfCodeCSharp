f188616abaa95b14f2a800e8a1d5c97783fba691
David Camp <Bpendragon@users.noreply.github.com>
2024-11-13 23:04:08 -0800

    Pre-Add solution files for 2024 season

---
+++ AdventOfCode/Solutions/Year2024/Day06-Solution.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using AdventOfCode.UserClasses;
+using System.Linq;
+using System.Data;
+using System.Threading;
+using System.Security;
+using static AdventOfCode.Solutions.Utilities;
+using System.Runtime.CompilerServices;
+
+namespace AdventOfCode.Solutions.Year2024
+{
+    [DayInfo(06, 2024, "")]
+    class Day06 : ASolution
+    {
+        public Day06() : base()
+        {
+
+        }
+
+        protected override object SolvePartOne()
+        {
+            return null;
+        }
+
+        protected override object SolvePartTwo()
+        {
+            return null;
+        }
+    }
+}
