cf7c4cd6b60118db7defef70133934b914bbbaac
David Camp <Bpendragon@users.noreply.github.com>
2024-11-30 21:17:45 -0800

    2024 Day 01, back in the saddle again.

--- AdventOfCode/Solutions/Year2024/Day01-Solution.cs
+++ AdventOfCode/Solutions/Year2024/Day01-Solution.cs
@@ -11,22 +11,32 @@ using System.Runtime.CompilerServices;
 
 namespace AdventOfCode.Solutions.Year2024
 {
-    [DayInfo(01, 2024, "")]
+    [DayInfo(01, 2024, "Historian Hysteria")]
     class Day01 : ASolution
     {
+        List<int> list1 = new();
+        List<int> list2 = new();
+
         public Day01() : base()
         {
-
+            foreach(var l in Input.SplitByNewline())
+            {
+                var ints = l.ExtractPosInts().ToArray();
+                list1.Add(ints[0]);
+                list2.Add(ints[1]);
+            }
+            list1.Sort();
+            list2.Sort();
         }
 
         protected override object SolvePartOne()
         {
-            return null;
+            return list1.Zip(list2).Sum(a => Math.Abs(a.First-a.Second));
         }
 
         protected override object SolvePartTwo()
         {
-            return null;
+            return list1.Sum(a => a * list2.Count(b => a == b));
         }
     }
 }
