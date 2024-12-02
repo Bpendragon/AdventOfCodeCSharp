5291ffc1d47c23ac3a773913965e249ea674ee24
David Camp <Bpendragon@users.noreply.github.com>
2024-12-01 21:54:09 -0800

    Day 02

--- AdventOfCode/Solutions/Year2024/Day02-Solution.cs
+++ AdventOfCode/Solutions/Year2024/Day02-Solution.cs
@@ -1,32 +1,77 @@
 using System;
-using System.Text;
 using System.Collections.Generic;
-using AdventOfCode.UserClasses;
 using System.Linq;
-using System.Data;
-using System.Threading;
-using System.Security;
-using static AdventOfCode.Solutions.Utilities;
-using System.Runtime.CompilerServices;
 
 namespace AdventOfCode.Solutions.Year2024
 {
-    [DayInfo(02, 2024, "")]
+    [DayInfo(02, 2024, "Red-Nosed Reports")]
     class Day02 : ASolution
     {
+        List<List<int>> levels = new();
+
         public Day02() : base()
         {
-
+            foreach(var n in Input.SplitByNewline())
+            {
+                levels.Add(n.ExtractInts().ToList());
+            }
         }
 
         protected override object SolvePartOne()
         {
-            return null;
+            int safeCount = 0;
+            foreach(var l in levels)
+            {
+                if (TestList(l)) safeCount++;
+            }
+            return safeCount;
         }
+
 
         protected override object SolvePartTwo()
         {
-            return null;
+            int safeCount = 0;
+
+            foreach(var l in levels)
+            {
+                for (int i = 0; i < l.Count; i++)
+                {
+                    List<int> n = new(l);
+                    n.RemoveAt(i);
+                    if (TestList(n))
+                    {
+                        safeCount++;
+                        break;
+                    }
+                }
+
+            }
+
+            return safeCount;
+        }
+
+
+        private bool TestList(List<int> l)
+        {
+            if (l[0] == l[1]) return false;
+            bool isIncreasing = l[0] < l[1];
+            for (int i = 0; i < l.Count - 1; i++)
+            {
+                if (l[i] == l[i + 1])
+                {
+                    return false;
+                }
+                if ((isIncreasing && l[i] > l[i + 1]) || (!isIncreasing && l[i] < l[i + 1]))
+                {
+                    return false;
+                }
+                if (Math.Abs(l[i] - l[i + 1]) > 3)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
