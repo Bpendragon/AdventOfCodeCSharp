2a1052a17b9afbef9e7f4eef4afddd45271f7a44
David Camp <Bpendragon@users.noreply.github.com>
2024-12-02 21:21:30 -0800

    2024 Day 3

--- AdventOfCode/Solutions/Year2024/Day03-Solution.cs
+++ AdventOfCode/Solutions/Year2024/Day03-Solution.cs
@@ -1,32 +1,45 @@
-using System;
-using System.Text;
 using System.Collections.Generic;
-using AdventOfCode.UserClasses;
 using System.Linq;
-using System.Data;
-using System.Threading;
-using System.Security;
-using static AdventOfCode.Solutions.Utilities;
-using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Solutions.Year2024
 {
-    [DayInfo(03, 2024, "")]
+    [DayInfo(03, 2024, "Mull It Over")]
     class Day03 : ASolution
     {
+        IEnumerable<Match> muls;
         public Day03() : base()
         {
-
+            muls = Regex.Matches(Input, "mul\\(\\d+,\\d+\\)");
         }
 
         protected override object SolvePartOne()
         {
-            return null;
+            long sum = 0;
+            foreach(Match m in muls)
+            {
+                var i = m.Value.ExtractLongs().ToArray();
+                sum += i[0] * i[1];
+            }
+            return sum;
         }
 
         protected override object SolvePartTwo()
         {
-            return null;
+            var enables = Regex.Matches(Input, "(do\\(\\)|don\\'t\\(\\))");
+
+            long sum = 0;
+
+            foreach (Match m in muls)
+            { 
+                string lastInstruction = enables.LastOrDefault(a => a.Index < m.Index)?.Value ?? "do()";
+                if (lastInstruction == "do()")
+                {
+                    var i = m.Value.ExtractLongs().ToArray();
+                    sum += i[0] * i[1];
+                }
+            }
+            return sum;
         }
     }
 }
