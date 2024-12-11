477a9384a5f2ad7fa3b31ffc2da5b5fa568e19b5
David Camp <Bpendragon@users.noreply.github.com>
2024-12-10 21:51:21 -0800

    2024 Day 11, full speed

--- AdventOfCode/Solutions/Year2024/Day11-Solution.cs
+++ AdventOfCode/Solutions/Year2024/Day11-Solution.cs
@@ -6,50 +6,24 @@ namespace AdventOfCode.Solutions.Year2024
     [DayInfo(11, 2024, "Plutonian Pebbles")]
     class Day11 : ASolution
     {
-        List<long> initialVals;
-        LinkedList<long> stones = new();
         DefaultDictionary<long, long> defaultDict= new();
-        
+        long p1 = 0;
+
+
         public Day11() : base()
         {
-            initialVals = Input.ExtractLongs().ToList();
-
-            foreach (var item in initialVals)
+            foreach (var item in Input.ExtractLongs())
             {
-                stones.AddLast(item);
                 defaultDict[item]++;
             }
         }
 
         protected override object SolvePartOne()
         {
-            for(int i = 0; i < 25; i++)
-            {
-                var curNode = stones.First;
-                while(true)
-                {
-                    if (curNode.Value == 0) curNode.Value = 1;
-                    else if (curNode.Value.ToString().Length % 2 == 0)
-                    {
-                        var tmp = curNode.Value.ToString();
-                        curNode.Value = int.Parse(tmp.Skip(tmp.Length / 2).JoinAsStrings());
-                        stones.AddBefore(curNode, int.Parse(tmp.Take(tmp.Length / 2).JoinAsStrings()));
-                    }
-                    else curNode.Value *= 2024;
-
-                    if (curNode.Next is null) break;
-                    curNode = curNode.Next;
-                }
-            }
-            return stones.Count;
-        }
-
-        protected override object SolvePartTwo()
-        {
-            for(int i = 0; i< 75; i++)
+            for (int i = 1; i <= 75; i++)
             {
                 DefaultDictionary<long, long> newData = new();
-                foreach(var kvp in defaultDict)
+                foreach (var kvp in defaultDict)
                 {
                     if (kvp.Key == 0) newData[1] += kvp.Value;
                     else if (kvp.Key.ToString().Length % 2 == 0)
@@ -61,9 +35,15 @@ namespace AdventOfCode.Solutions.Year2024
                     else newData[kvp.Key * 2024] += kvp.Value;
                 }
                 defaultDict = newData;
+                if (i == 25) p1 = defaultDict.Sum(a => a.Value);
             }
-            return defaultDict.Sum(a => a.Value);
+
+            return p1;
         }
 
+        protected override object SolvePartTwo()
+        {
+            return defaultDict.Sum(a => a.Value);
+        }
     }
 }
