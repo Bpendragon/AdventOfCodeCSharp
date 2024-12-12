8d5f9c703b49975af3c9cd9e26f300cc1ea25503
David Camp <Bpendragon@users.noreply.github.com>
2024-12-11 23:23:43 -0800

    speed up 2024 day 1 by nearly 25 ms

--- AdventOfCode/Solutions/Year2024/Day01-Solution.cs
+++ AdventOfCode/Solutions/Year2024/Day01-Solution.cs
@@ -9,6 +9,8 @@ namespace AdventOfCode.Solutions.Year2024
     {
         List<int> list1 = new();
         List<int> list2 = new();
+        DefaultDictionary<int, int> counts1 = new();
+        DefaultDictionary<int, int> counts2 = new();
 
         public Day01() : base()
         {
@@ -17,6 +19,8 @@ namespace AdventOfCode.Solutions.Year2024
                 var ints = l.ExtractPosInts().ToArray();
                 list1.Add(ints[0]);
                 list2.Add(ints[1]);
+                counts1[ints[0]]++;
+                counts2[ints[1]]++;
             }
             list1.Sort();
             list2.Sort();
@@ -29,7 +33,7 @@ namespace AdventOfCode.Solutions.Year2024
 
         protected override object SolvePartTwo()
         {
-            return list1.Sum(a => a * list2.Count(b => a == b));
+            return counts1.Sum(a => a.Value * a.Key * counts2[a.Key]);
         }
     }
 }
