ed8b8ce5c56a57febdaa9e8fdbdc0b4ce76ebe14
David Camp <Bpendragon@users.noreply.github.com>
2024-12-13 23:17:52 -0800

    2024 Day 14, in the worst possible way

--- AdventOfCode/Solutions/Year2024/Day14-Solution.cs
+++ AdventOfCode/Solutions/Year2024/Day14-Solution.cs
@@ -1,32 +1,102 @@
-using System;
-using System.Text;
 using System.Collections.Generic;
-using AdventOfCode.UserClasses;
-using System.Linq;
 using System.Data;
-using System.Threading;
-using System.Security;
-using static AdventOfCode.Solutions.Utilities;
-using System.Runtime.CompilerServices;
+using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2024
 {
-    [DayInfo(14, 2024, "")]
+    [DayInfo(14, 2024, "Restroom Redoubt")]
     class Day14 : ASolution
     {
+        int maxX = 101;
+        int maxY = 103;
+
+        List<Robot> robots = new();
+
         public Day14() : base()
         {
+            foreach(var p in Input.SplitByNewline())
+            {
+                var l = p.ExtractInts().ToList();
 
+                Robot r = new();
+                r.startingLoc = (l[0], l[1]);
+                r.startingVelocity = (l[2], l[3]);
+                r.roomWidth = maxX;
+                r.roomHeight = maxY;
+
+                robots.Add(r);
+            }
         }
 
         protected override object SolvePartOne()
         {
-            return null;
+            DefaultDictionary<int, int> quads = new();
+            foreach(var r in robots)
+            {
+                r.Move(100);
+                quads[r.Quadrant]++;
+            }
+
+            return quads.Where(a => a.Key > 0).Select(a => a.Value).Aggregate(1, (a, b) => a * b);
         }
 
         protected override object SolvePartTwo()
         {
-            return null;
+            int count = 0;
+            while (true) 
+            {
+                HashSet<Coordinate2D> robotLocs = new();
+                bool isUnique = true;
+                foreach(var r in robots)
+                {
+                    r.Move(count);
+                    if(!robotLocs.Add(r.curLoc))
+                    {
+                        isUnique = false;
+                        break;
+                    }
+                }
+                if (isUnique) break;
+                count++;
+            }
+
+            return count;
+
+        }
+
+        private class Robot
+        {
+            public Coordinate2D startingLoc { get; set; }
+            public Coordinate2D startingVelocity { get; set; }
+            public Coordinate2D curLoc { get; private set; }
+            public int roomWidth { get; set; }
+            public int roomHeight { get; set; }
+            public int Quadrant => this.GetQuadrant();
+
+            public void Move(int steps)
+            {
+                curLoc = startingLoc + (steps * startingVelocity);
+                curLoc = (curLoc.x % roomWidth, curLoc.y % roomHeight);
+                if (curLoc.x < 0) curLoc = (roomWidth + curLoc.x , curLoc.y);
+                if (curLoc.y < 0) curLoc = (curLoc.x, roomHeight + curLoc.y);
+            }
+
+            public void Reset()
+            {
+                curLoc = startingLoc;
+            }
+
+            private int GetQuadrant()
+            {
+                if (curLoc.x > (roomWidth) / 2 && curLoc.y > (roomHeight) / 2) return 1;
+                else if (curLoc.x < (roomWidth) / 2 && curLoc.y > (roomHeight) / 2) return 2;
+                else if (curLoc.x < (roomWidth) / 2 && curLoc.y < (roomHeight) / 2) return 3;
+                else if (curLoc.x > (roomWidth) / 2 && curLoc.y < (roomHeight) / 2) return 4;
+
+                return 0;
+
+            }
+
         }
     }
 }
