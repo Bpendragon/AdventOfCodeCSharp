40e876347efe093f4fd3f677e3e2907f9f8c19ae
David Camp <Bpendragon@users.noreply.github.com>
2024-12-18 22:03:14 -0800

    2024 Day 19

--- AdventOfCode/Solutions/Year2024/Day19-Solution.cs
+++ AdventOfCode/Solutions/Year2024/Day19-Solution.cs
@@ -1,34 +1,71 @@
-using AdventOfCode.UserClasses;
-
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Security;
-using System.Text;
-using System.Threading;
-
-using static AdventOfCode.Solutions.Utilities;
 
 namespace AdventOfCode.Solutions.Year2024
 {
-    [DayInfo(19, 2024, "")]
+    [DayInfo(19, 2024, "Linen Layout")]
     class Day19 : ASolution
     {
+        HashSet<string> towels = new();
+        List<string> patterns;
+        Dictionary<string, bool> memo = new();
+        DefaultDictionary<string, long> counts = new();
+        long p2res = 0;
+
         public Day19() : base()
         {
-
+            var halves = Input.SplitByDoubleNewline();
+            towels = new(halves[0].Split([',', ' '], StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries));
+            patterns = new(halves[1].SplitByNewline());
         }
 
         protected override object SolvePartOne()
         {
-            return null;
+            int count = 0;
+
+            foreach(var pattern in patterns)
+            {
+                (bool succ, long resCount) = DFS(pattern);
+                if (succ)
+                {
+                    count++;
+                    p2res += resCount;
+                }
+            }
+
+            return count;
         }
 
         protected override object SolvePartTwo()
         {
-            return null;
+            return p2res;
+        }
+
+        private (bool works, long ways) DFS(string pattern)
+        {
+            if (memo.ContainsKey(pattern)) return (memo[pattern], counts[pattern]);
+            if (towels.Contains(pattern))
+            {
+                memo[pattern] = true;
+                counts[pattern]++;
+            }
+
+            foreach(var t in towels.Where(t => t.Length < pattern.Length && pattern.Substring(0, t.Length) == t))
+            {
+                string sub = pattern.Substring(t.Length);
+                var res = DFS(sub);
+                if (res.works)
+                {
+                    memo[pattern] = true;
+                    counts[pattern] += res.ways;
+                }
+            }
+
+            memo[pattern] = counts[pattern] > 0;
+
+            return (memo[pattern], counts[pattern]);
         }
     }
 }
