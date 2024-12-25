9751923689b0d1ecffeb5c6ce353bf0561467be8
David Camp <Bpendragon@users.noreply.github.com>
2024-12-24 21:14:16 -0800

    2024 Day 2025 ❄️🎄Happy Advent of Code🎄❄️

--- AdventOfCode/Solutions/Year2024/Day25-Solution.cs
+++ AdventOfCode/Solutions/Year2024/Day25-Solution.cs
@@ -1,29 +1,49 @@
-﻿using AdventOfCode.UserClasses;
-
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Security;
-using System.Text;
-using System.Threading;
-
-using static AdventOfCode.Solutions.Utilities;
 
 namespace AdventOfCode.Solutions.Year2024
 {
-    [DayInfo(25, 2024, "")]
+    [DayInfo(25, 2024, "Code Chronicle")]
     class Day25 : ASolution
     {
+        List<List<int>> locks = new();
+        List<List<int>> keys = new();
         public Day25() : base()
         {
-
+            foreach(var schema in Input.SplitByDoubleNewline())
+            {
+                if (schema[0] == '.') //It's a key
+                {
+                    List<int> t = new();
+                    foreach(var c in schema.SplitIntoColumns())
+                    {
+                        t.Add(c.Count(a => a == '#') - 1);
+                    }
+                    keys.Add(t);
+                } else //it's a lock
+                {
+                    List<int> t = new();
+                    foreach (var c in schema.SplitIntoColumns())
+                    {
+                        t.Add(c.Count(a => a == '#') - 1);
+                    }
+                    locks.Add(t);
+                }
+            }
         }
 
         protected override object SolvePartOne()
         {
-            return null;
+            int count = 0;
+            foreach(var k in keys)
+            {
+                foreach(var l in locks)
+                {
+                    if (l.Zip(k).All(a => a.First + a.Second < 6)) count++;
+                }
+            }
+            return count;
         }
 
         protected override object SolvePartTwo()
