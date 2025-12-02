d5aa5cd0a41820b5bd7a9cdb8900293be16db8da
David Camp <Bpendragon@users.noreply.github.com>
2025-12-01 22:27:02 -0800

    now it's a one-shot

--- AdventOfCode/Solutions/Year2025/Day02-Solution.cs
+++ AdventOfCode/Solutions/Year2025/Day02-Solution.cs
@@ -7,53 +7,49 @@ namespace AdventOfCode.Solutions.Year2025
     [DayInfo(02, 2025, "Gift Shop")]
     class Day02 : ASolution
     {
-        List<(long s, long e)> ranges = new(); 
+        long p1Res = 0;
+        long p2Res = 0;
 
         public Day02() : base()
         {
-            var groups = Input.ExtractPosLongs().ToArray();
-            for(int i = 0; i < groups.Length; i+=2)
+            var groups = Input.ExtractPosLongs();
+            var _enum = groups.GetEnumerator();
+            while (_enum.MoveNext())
             {
-                ranges.Add((groups[i], groups[i + 1]));
-            }
-        }
-
-        protected override object SolvePartOne()
-        {
-            long res = 0;
-            foreach (var g in ranges)
-            {
-                for (long i = g.s; i <= g.e; i++)
+                long s = _enum.Current;
+                _enum.MoveNext();
+                long e = _enum.Current;
+                for (long i = s; i <= e; i++)
                 {
                     var cur = i.ToString();
-                    if (cur.Length % 2 == 1) continue;
-                    if (cur.Substring(0, cur.Length / 2) == cur.Substring(cur.Length / 2)) res += i;
-                }
-            }
-            return res;
-        }
-
-        protected override object SolvePartTwo()
-        {
-            long res = 0;
-            foreach (var g in ranges)
-            {
-                for (long i = g.s; i <= g.e; i++)
-                {
-                    var cur = i.ToString();
-                    for (int j = 1; j <= cur.Length / 2; j++)
+                    for (int j = cur.Length / 2; j >= 1; j--)
                     {
                         var chunks = cur.StringChunks(j);
                         if (chunks.All(x => x == chunks.First()))
                         {
-                            res += i;
+                            if (j == cur.Length / 2 && cur.Length % 2 == 0)
+                            {
+                                p1Res += i;
+                            }
+                            else
+                            {
+                                p2Res += i;
+                            }
                             break;
                         }
                     }
                 }
             }
+        }
 
-            return res;
+        protected override object SolvePartOne()
+        {
+            return p1Res;
+        }
+
+        protected override object SolvePartTwo()
+        {
+            return p1Res + p2Res;
         }
     }
 }
