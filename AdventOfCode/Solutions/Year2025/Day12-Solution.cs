70793dbddc999053686ae456b2b3189dfc8c99f1
David Camp <Bpendragon@users.noreply.github.com>
2025-12-11 21:59:30 -0800

    2025 Day 12, ❄️🎄Happy Advent of Code🎄❄️ Eric You Troll!

--- AdventOfCode/Solutions/Year2025/Day12-Solution.cs
+++ AdventOfCode/Solutions/Year2025/Day12-Solution.cs
@@ -1,27 +1,39 @@
 ﻿using System;
-using System.Text;
 using System.Collections.Generic;
-using AdventOfCode.UserClasses;
 using System.Linq;
-using System.Data;
-using System.Threading;
-using System.Security;
-using static AdventOfCode.Solutions.Utilities;
-using System.Runtime.CompilerServices;
 
 namespace AdventOfCode.Solutions.Year2025
 {
-    [DayInfo(12, 2025, "")]
+    [DayInfo(12, 2025, "Christmas Tree Farm")]
     class Day12 : ASolution
     {
+        List<int> p = new();
+        int res = 0;
         public Day12() : base()
         {
+            var sections = Input.SplitByDoubleNewline();
+            foreach(var s in sections.SkipLast(1))
+            {
+                p.Add(s.Count(a => a == '#'));
+            }
 
+            foreach(var region in sections.Last().SplitByNewline())
+            {
+                var r = region.ExtractInts().ToArray();
+                res += r[0] * r[1] > 
+                        (r[2] * p[0]) 
+                        + (r[3] * p[1]) 
+                        + (r[4] * p[1]) 
+                        + (r[5] * p[3]) 
+                        + (r[6] * p[4]) 
+                        + (r[7] * p[5]) 
+                        ? 1 : 0;
+            }
         }
 
         protected override object SolvePartOne()
         {
-            return null;
+            return res;
         }
 
         protected override object SolvePartTwo()
